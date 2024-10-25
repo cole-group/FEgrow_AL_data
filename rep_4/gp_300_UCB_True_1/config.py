9a96b6aaf6991b596788be8178ca00559b6ea863
djcole56 <daniel.cole@newcastle.ac.uk>
2024-10-25 13:34:12 +0100

    Add data

---
+++ rep_4/gp_300_UCB_True_1/config.py
@@ -0,0 +1,32 @@
+cycle_dir: ''
+diverse: true
+metadata: Small test for active learning.
+model_config:
+  features:
+    feature_type: fingerprint
+    params:
+      feature_column: Smiles
+      fingerprint_radius: 3
+      fingerprint_size: 2048
+  hyperparameters: {}
+  model_type: gp
+  targets:
+    feature_type: number
+    params:
+      feature_column: cnnaffinity
+  tuning_hyperparameters:
+  - {}
+selection_config:
+  hyperparameters: {}
+  num_elements: 300
+  selection_columns:
+  - cnnaffinity
+  - Smiles
+  - h
+  - fid
+  selection_type: UCB
+  tuning_hyperparameters:
+  - beta: 1
+training_pool: ''
+virtual_library: manual_init_h6_rgroups_linkers100_scorable.csv
+time,1804.5092504024506,s
